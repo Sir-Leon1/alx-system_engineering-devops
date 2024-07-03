99a5a6851e45753e2df8418891203560eea7334c
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-07-03 13:01:19 +0300

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
 # Install specific  version of flask using pip3
 
+# Install Flask version
 package { 'Flask':
-ensure   => '2.1.0',
-provider => 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+# Install pip for python 3
+package { 'python3-pip':
+  ensure  => 'installed',
+}
+
+# Install Werkzeug 2.1.1 using pip
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
