880e5143edf5f1baebf8a797c39a5ff7a38a1807
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-07-03 11:36:35 +0300

    Task 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,12 @@
 # Install specific  version of flask using pip3
 
-node 'flask' {
-  python::pip { 'flask':
-    ensure       => '2.1.0',
-    pip_provider => 'pip3',
-  }
+package { 'python3-pip':
+ensure   => installed,
+}
+
+exec { 'install-flask':
+command => '/bin/pip3 install Flask==2.1.0',
+creates => '/usr/local/lib/python3.8/dist-packages/flask/__init__.py',
+require => Package['python3-pip'],
+path => ['/usr/bin', '/usr/local/bin'],
 }
