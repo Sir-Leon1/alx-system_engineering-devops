a200f0690f7118e7b1f4b09427aca263f17dd0b7
Gunnah Leon Agandi <114517374+Sir-Leon1@users.noreply.github.com>
2024-07-03 07:54:23 +0300

    Update 1-install_a_package.pp
    Changed the install method

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
 # Install specific  version of flask using pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+node 'flask' {
+  python::pip { 'flask':
+    ensure       => '2.1.0',
+    pip_provider => 'pip3',
+  }
 }
