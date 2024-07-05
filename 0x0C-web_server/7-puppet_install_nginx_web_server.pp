a0a7b9be02a85f5103585891189c913e98b1fde4
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-07-05 11:15:04 +0300

    4 and 5

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,51 @@
+#nginx setup
+
+#Ensure Nginx package is installed and service is running
+package { 'nginx':
+  ensure => installed,
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
+# Define directory and file resources for HTML content
+file { '/etc/nginx/html':
+  ensure => directory,
+}
+
+file { '/etc/nginx/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+}
+
+file { '/etc/nginx/html/404.html':
+  ensure  => file,
+  content => "Ceci n'est pas une page",
+}
+
+#Nginx server configuration
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => @(END),
+server {
+  listen  80 default_server;
+  listen  [::]: 80 deefault_server;
+  root    /etc / nginx / html;
+  index   index.html;
+
+  location /redirect_me {
+    return 301 http://cuberule.com;
+  }
+
+  error_page 404 /404.html;
+  location /404 {
+    root /etc/nginx/html;
+    internal;
+  }
+}
+END
+notify    => Service['nginx'],
+}
