2b382996b3976330898200f04f481d13f0562c79
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-07-13 17:39:17 +0300

    Task 2 again

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,29 +2,29 @@
 # and custom HTTP header
 
 exec { 'update sys':
-	command => '/usr/bin/apt-get update',
+  command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
-	ensure => 'installed',
-	require => Exec['update sys']
+  ensure  => 'installed',
+  require => Exec['update sys']
 }
 
 file {'/var/www/html/index.html':
-	content => 'Hello World!'
+  content => 'Hello World!'
 }
 
 exec {'redirect_me':
-	command => 'sed -i "24i\	rewrite ^/redirect_me http://cuberule.com/ permanent;" /etc/nginx/sites-available/default',
-	provider => 'shell'
+  command  => 'sed -i "24i\	rewrite ^/redirect_me http://cuberule.com/ permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell'
 }
 
 exec {'HTTP head':
-	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
-	provider => 'shell'
+  command  => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+  provider => 'shell'
 }
 
 service {'nginx':
-	ensure => running,
-	require => Package['nginx']
+  ensure  => running,
+  require => Package['nginx']
 }
