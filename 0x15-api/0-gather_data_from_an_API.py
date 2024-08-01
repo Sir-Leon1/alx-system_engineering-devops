056ec38afa7cfdcef11d02935f40e837b51fd939
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-08-01 18:59:38 +0300

    Correction on 0 and 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     todo = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
     completed = [t.get("title") for t in todo if t.get("completed") is True]
-    print("Employee {} is done with tasks{}/{}:".format(
+    print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todo)
     ))
     [print("\t {}".format(c)) for c in completed]
