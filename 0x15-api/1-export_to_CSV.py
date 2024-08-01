1ea8bf8d0ef5375cd84b53518cdf6a291529e6e5
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-08-01 18:47:08 +0300

    All tasks

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Export to-do list info for given employeee ID to CSV format."""
+import csv
+import requests
+import sys
+
+from urllib3.filepost import writer
+
+if __name__ == "__mani__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        write = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+        ) for t in todos]
