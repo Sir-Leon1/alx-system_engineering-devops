71c118649b7f7d16a692823b8e8c2ac53e9fee64
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-09-25 11:27:47 +0300

    Local changes

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,8 +3,13 @@
 # Increase the ULIMIT of the default file
 exec {'fix--for-nginx :'
 command => 'sed -i "s/15/4096/" /etc/default/nginx',
-path => '/usr/local/bin/:/bin/'
+path => '/usr/local/bin/'
 }
+
+exec { 'Edit-nginx-init-script',
+command => 
+}
+
 
 # Restart Nginx
 -> exec { 'nginx-restart',
