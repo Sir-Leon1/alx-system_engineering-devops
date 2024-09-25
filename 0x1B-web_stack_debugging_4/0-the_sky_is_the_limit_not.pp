22bce25a7f62225949ab3a43b7e3f377f9c35c61
Gunnah Leon Agandi <114517374+Sir-Leon1@users.noreply.github.com>
2024-09-25 11:25:59 +0300

    Corrected syntax on the puppet file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,12 @@
-# Increase the amount of traffic an Nginx server can handle
-
 # Increase the ULIMIT of the default file
-exec {'fix--for-nginx :'
-command => 'sed -i "s/15/4096/" /etc/default/nginx',
-path => '/usr/local/bin/:/bin/'
+exec { 'fix-for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => ['/usr/local/bin', '/bin', '/usr/bin'],
 }
 
 # Restart Nginx
--> exec { 'nginx-restart',
-command => 'nginx restart',
-path => 'etc/init.d/'
+exec { 'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+  require => Exec['fix-for-nginx'],  # Ensure this runs after the fix
 }
